b4797bd2311876ffec55c03e70220ed4f3b4a5bb
nphuong06 <nguyenphuong2006123@gmail.com>
2024-12-20 12:27:17 +0700

    Add files via upload

---
+++ ex4-ss18.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+struct Student {
+    int id;
+    char name[100];
+    int age;
+    char phoneNumber[15];
+};
+int main() {
+    struct Student students[50];
+    for (int i = 0; i < 5; i++) {
+        students[i].id = i + 1;
+        printf("Nhap thong tin sinh vien %d:\n", i + 1);
+        printf("Nhap ten sinh vien: ");
+        fgets(students[i].name, sizeof(students[i].name), stdin);
+        printf("Nhap tuoi sinh vien: ");
+        scanf("%d", &students[i].age);
+        getchar();
+        printf("Nhap so dien thoai sinh vien: ");
+        fgets(students[i].phoneNumber, sizeof(students[i].phoneNumber), stdin);
+        printf("\n");
+    }
+    printf("Thong tin cac sinh vien:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Sinh vien %d:\n", students[i].id);
+        printf("Ten: %s", students[i].name);
+        printf("Tuoi: %d\n", students[i].age);
+        printf("So dien thoai: %s", students[i].phoneNumber);
+        printf("\n");
+    }
+    return 0;
+}
+
