33392b5c0c4f76ad5b0f7b368d4c0bc2c7181a4d
nphuong06 <nguyenphuong2006123@gmail.com>
2024-12-20 12:26:42 +0700

    Add files via upload

---
+++ ex2-ss18.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+struct Student {
+    char name[100];
+    int age;
+    char phoneNumber[15];
+};
+int main() {
+    struct Student student;
+      printf("Nhap ten sinh vien: ");
+      fgets(student.name, sizeof(student.name), stdin);
+      printf("Nhap tuoi sinh vien: ");
+      scanf("%d", &student.age);
+      getchar();
+      printf("Nhap so dien thoai sinh vien: ");
+      fgets(student.phoneNumber, sizeof(student.phoneNumber), stdin);
+      printf("\nThong tin sinh vien:\n");
+      printf("Ten sinh vien: %s", student.name);
+      printf("Tuoi sinh vien: %d\n", student.age);
+      printf("So dien thoai sinh vien: %s", student.phoneNumber);
+    return 0;
+}
+
